f9c48ba3c1788529a8a4907b4757ccaf38683f6a
minkyeong kim <mgk04222@naver.com>
2023-07-30 23:03:27 +0900

    퀴즈

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNo = "상암08번";
+        String minutes = "3";
+        String distance = "1.5";
+        System.out.println(busNo + "버스");
+        System.out.println("약 " + minutes + "분 후 도착");
+        System.out.println("남은 거리 "+ distance + "km");
+    }
+
+}
